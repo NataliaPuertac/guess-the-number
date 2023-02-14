b53d9d8187607c78cba3aa77d7678ae8609c4247
NataliaPuertac <nataliapuertac@gmail.com>
2023-02-14 10:08:56 +0100

    add compare function

--- js/main.js
+++ js/main.js
@@ -2,6 +2,40 @@
 
 //constantes
 
+const userNumber = document.querySelector('.js-label');
+const btn = document.querySelector('.js-btn');
+const pista = document.querySelector('.js-pista');
+const intentos = document.querySelector('.js-intentos');
+const randomNumber = getRandomNumber(100);
+
+console.log(`el número aleatorio es ${randomNumber}`);
+
 //funciones
 
-//eventos
+function getRandomNumber(max) {
+  return Math.ceil(Math.random() * max);
+}
+
+function compare() {
+  const userNumberValue = userNumber.value;
+  if (parseInt(userNumberValue) === randomNumber) {
+    pista.innerHTML = `Has ganado campeona!!!`;
+  } else if (parseInt(userNumberValue) === 0) {
+    pista.innerHTML = `El número debe estar entre 1 y 100.`;
+  } else if (parseInt(userNumberValue) > 100) {
+    pista.innerHTML = `El número debe estar entre 1 y 100.`;
+  } else if (parseInt(userNumberValue) < randomNumber) {
+    pista.innerHTML = `El número es demasiado bajo`;
+  } else if (parseInt(userNumberValue) > randomNumber) {
+    pista.innerHTML = `El número es demasiado alto`;
+  }
+}
+
+//eventos y función manejadora
+
+function handleclick(ev) {
+  ev.preventDefault();
+  compare();
+}
+
+btn.addEventListener('click', handleclick);
