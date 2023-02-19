7823ed9fe0bf78da307ab6f6aae9b36d5181644a
NataliaPuertac <nataliapuertac@gmail.com>
2023-02-19 15:31:52 +0100

    some improvements

--- js/main.js
+++ js/main.js
@@ -6,38 +6,49 @@ const userNumber = document.querySelector('.js-label');
 const btn = document.querySelector('.js-btn');
 const clue = document.querySelector('.js-clue');
 const tried = document.querySelector('.js-tried');
+const reset = document.querySelector('.js-reset');
 const randomNumber = getRandomNumber(100);
 let counter = 0;
 
 console.log(`el número aleatorio es ${randomNumber}`);
 
-//funciones
+//función para agrupar todos los innerHTML y no repetir
+
+function msg(message) {
+  clue.innerHTML = message;
+}
+
+//función para crear un numero aleatorio
 
 function getRandomNumber(max) {
   return Math.ceil(Math.random() * max);
 }
 
+//función para actualizar el contador de intentos
+
 function count() {
   counter = counter + 1;
-  tried.innerHTML = `Número de intentos = ${counter}`;
+  tried.innerHTML = `Número de intentos : ${counter}`;
 }
 
+// función comparadora de resultados
+
 function compare() {
-  const userNumberValue = userNumber.value;
-  if (parseInt(userNumberValue) === randomNumber) {
-    clue.innerHTML = `Has ganado campeona!!!`;
-  } else if (parseInt(userNumberValue) === 0) {
-    clue.innerHTML = `El número debe estar entre 1 y 100.`;
-  } else if (parseInt(userNumberValue) > 100) {
-    clue.innerHTML = `El número debe estar entre 1 y 100.`;
-  } else if (parseInt(userNumberValue) < randomNumber) {
-    clue.innerHTML = `El número es demasiado bajo`;
-  } else if (parseInt(userNumberValue) > randomNumber) {
-    clue.innerHTML = `El número es demasiado alto`;
+  const userNumberValue = parseInt(userNumber.value);
+  if (isNaN(userNumberValue)) {
+    msg(`Debes introducir un número`);
+  } else if (userNumberValue < 1 || userNumberValue > 100) {
+    msg(`El número debe estar entre 1 y 100.`);
+  } else if (userNumberValue < randomNumber) {
+    msg(`El número es demasiado bajo`);
+  } else if (userNumberValue > randomNumber) {
+    msg(`El número es demasiado alto`);
+  } else {
+    msg(`Has ganado campeona!!!`);
   }
 }
 
-//eventos y función manejadora
+//función manejadora
 
 function handleclick(ev) {
   ev.preventDefault();
@@ -45,4 +56,16 @@ function handleclick(ev) {
   count();
 }
 
+//función reset para refrescar la página, generar un nuevo número aleatorio y dejar el contador de nuevo en cero
+
+function handleClickReset(ev) {
+  counter = 0;
+  tried.innerHTML = `Número de intentos: ${counter}`;
+  userNumber.value = '';
+  location.reload();
+}
+
+//eventos
+
 btn.addEventListener('click', handleclick);
+reset.addEventListener('click', handleClickReset);
